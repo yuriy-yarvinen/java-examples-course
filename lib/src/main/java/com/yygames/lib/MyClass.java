5e0381a854fcae6bf6611d6097497298acf1515c
Yuriy Yarvinen <yuriy.yarvi@gmail.com>
2024-05-18 23:34:13 +0300

    Java init

---
+++ lib/src/main/java/com/yygames/lib/MyClass.java
@@ -0,0 +1,8 @@
+package com.yygames.lib;
+
+public class MyClass {
+    public static void main(String[] args){
+        /// java output
+        System.out.print("Hello World");
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Java examples course"
+include(":app")
+include(":lib")
