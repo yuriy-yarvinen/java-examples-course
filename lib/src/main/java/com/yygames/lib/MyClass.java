7bdcae0ee7a40118c84ec901c6e2e7f1f59f6313
Yuriy Yarvinen <yuriy.yarvi@gmail.com>
2024-05-19 00:00:37 +0300

    Operators

--- lib/src/main/java/com/yygames/lib/MyClass.java
+++ lib/src/main/java/com/yygames/lib/MyClass.java
@@ -21,5 +21,39 @@ public class MyClass {
         // Long: -9223372036854775808 --> 9223372036854775807
         Long w = 1122334455667788L;
 
+        // float: you should add "F" or "f" at the end of the number
+        float pi = 3.14F;
+
+        // double: store decimal numbers with higher precision
+        double pi2 = 3.14159;
+
+        // boolean: true or false
+        boolean isRaining = true;
+        boolean allCoinsCollected = false;
+
+
+        // char: holds only one single number, symbol or letter
+        char myChar = 'A';
+        char myNumber = '7';
+        char mySymbol = '$';
+        char newLine = '\n';
+        char tab = '\t' ;
+
+
+
+        // Arithmetic Operators: + *1%
+        System.out.println(10%5);
+
+        // Comparison Operators: = > < >= <=
+        int xx = 10;
+        System.out.println(xx != 9999);
+
+
+        // Logical Operators: && || !
+        boolean isSunny = true;
+        boolean needUmbrella = false;
+        boolean shouldGoOutside = isSunny && !needUmbrella;
+        boolean canPlayGames = isSunny || needUmbrella;
+
     }
 }
