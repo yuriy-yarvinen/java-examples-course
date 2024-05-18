dc07d10f531bc92595e13d44f4b564f35861f43a
Yuriy Yarvinen <yuriy.yarvi@gmail.com>
2024-05-18 23:45:44 +0300

    numbers

--- lib/src/main/java/com/yygames/lib/MyClass.java
+++ lib/src/main/java/com/yygames/lib/MyClass.java
@@ -3,6 +3,23 @@ package com.yygames.lib;
 public class MyClass {
     public static void main(String[] args){
         /// java output
-        System.out.print("Hello World");
+        System.out.print("Hello World \n");
+
+        int age = 25;
+
+        System.out.print("Hello World " + age);
+
+        // byte: -128 --> 127
+        byte x = 100;
+
+        // short -32,768 -> 32,767
+        short y = 7777;
+
+        // int: -2147483648 --> 2147483647
+        int z = 88776655;
+
+        // Long: -9223372036854775808 --> 9223372036854775807
+        Long w = 1122334455667788L;
+
     }
 }
